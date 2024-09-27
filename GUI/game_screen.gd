13c792dbc404297a4ba7958dc9dddeb44434abb1
Noah <noahgillam007@gmail.com>
2024-09-27 12:53:59 +1200

    showabove

--- GUI/game_screen.gd
+++ GUI/game_screen.gd
@@ -8,16 +8,7 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("pause"):
-		toggle_pause()
+		GameManager.pause_game()
 
 func on_collectable_award_recieved(total_award : int): 
 	collectable_label.text = str(total_award)
-
-# Toggle the pause state and handle visibility
-func toggle_pause():
-	if GameManager.is_paused:  # Check if the game is already paused
-		GameManager.resume_game()  # Resume the game
-		collectable_label.visible = true  # Show the UI when resuming
-	else:
-		GameManager.pause_game()  # Pause the game
-		collectable_label.visible = false  # Hide the UI when paused
